3ddaa01dfd04a660d511fe4fda15a539daf7c795
unknown <jenha04@gmail.com>
2022-06-06 21:59:02 -0500

    Version1.0

---
+++ VeterinariaFinal/Controllers/ProductoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VeterinariaFinal.Models;
+
+namespace VeterinariaFinal.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            List<producto> listado;
+            using (var db = new VETERINARIAEntities())
+            {
+                listado = db.producto.ToList();
+            }
+            return View(listado);
+        }
+    }
+}
---
+++ VeterinariaFinal/Controllers/VeterinarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VeterinariaFinal.Models;
+
+namespace VeterinariaFinal.Controllers
+{
+    public class VeterinarioController : Controller
+    {
+        // GET: Veterinario
+        public ActionResult Index()
+        {
+            List<veterinario> listado;
+            using(var db = new VETERINARIAEntities())
+            {
+                listado = db.veterinario.ToList();
+            }
+            return View(listado);
+        }
+    }
+}
