f3e5a0bc2e51babf516fc7f3e0b72476c4bc79d5
unknown <jenha04@gmail.com>
2022-06-07 21:49:33 -0500

    Version2.0

--- VeterinariaFinal/Controllers/ProductoController.cs
+++ VeterinariaFinal/Controllers/ProductoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +10,8 @@ namespace VeterinariaFinal.Controllers
 {
     public class ProductoController : Controller
     {
+        
+
         // GET: Producto
         public ActionResult Index()
         {
@@ -19,5 +22,66 @@ namespace VeterinariaFinal.Controllers
             }
             return View(listado);
         }
+
+        public ActionResult Editar(int id)
+        {
+            producto prod = new producto();
+
+            using(var db = new VETERINARIAEntities())
+            {
+                prod = db.producto.Find(id);
+            }
+            return View(prod);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(producto prod)
+        {
+            var prodEdit = new producto() { idProducto = prod.idProducto, nombreProd = prod.nombreProd, precio = prod.precio };
+
+            using (var db = new VETERINARIAEntities())
+            {
+                db.producto.Attach(prodEdit);
+                db.Entry(prodEdit).Property(x => x.nombreProd).IsModified = true;
+                db.Entry(prodEdit).Property(x => x.precio).IsModified = true;
+                db.SaveChanges();
+
+            }
+            return RedirectToAction("Index", "Producto");
+        }
+
+
+        public ActionResult Crear()
+        {
+            using (var db = new VETERINARIAEntities())
+            {
+                List<categoria> lcategoria = new List<categoria>();
+                lcategoria = db.categoria.ToList();
+                ViewBag.listacategoria = lcategoria;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Crear(producto prod, System.Web.UI.WebControls.FileUpload fuFoto)
+        {
+            System.Diagnostics.Debug.WriteLine("name:" + fuFoto.FileName);
+
+            return RedirectToAction("Index", "Producto");
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(int id)
+        {
+            using(var db = new VETERINARIAEntities())
+            {
+                var producto = new producto { idProducto = id};
+                db.Entry(producto).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "Producto");
+        }
+
     }
 }
